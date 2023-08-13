c950f9c1b04671a8c59b36ed82f17c222f1d91a0
DrCodeMonster <tarikeser@gmail.com>
2023-08-13 15:20:36 +0300

    package upgraded

--- src/hooks/useSetState.ts
+++ src/hooks/useSetState.ts
@@ -1,12 +1,11 @@
 import { useCallback, useState } from 'react';
 
-// eslint-disable-next-line
 const useSetState = <T extends object>(
     initialState: T = {} as T
 ): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
     const [state, set] = useState<T>(initialState);
     const setState = useCallback(
-        (patch) => {
+        (patch: any) => {
             set((prevState) => ({
                 ...prevState,
                 ...(patch instanceof Function ? patch(prevState) : patch),
